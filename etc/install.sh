f9382f1d26bf9d1bca07a54d1fc208900e7f3846
lighttiger <lighttiger2505@gmail.com>
2017-08-02 23:55:48 +0900

    Rename setup scripts.

--- etc/install.sh
+++ etc/install.sh
@@ -2,10 +2,10 @@
 
 if [ "$(uname)" == 'Darwin' ]; then
     # Mac
-    ~/.dotfiles/bin/mac/install-brew.sh
+    ./install-brew.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
     # Linux
-    ~/.dotfiles/bin/linux/install-apt.sh
+    ./install-apt.sh
 else
     echo "Your platform ($(uname -a)) is not supported."
     exit 1
