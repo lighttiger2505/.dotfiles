f259f2276e41e4e040b72a939a1ea888b78d0a25
lighttiger <lighttiger2505@gmail.com>
2017-09-08 01:56:41 +0900

    Update func of fzf call
    shell state of not waiting input command when cancel

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -9,8 +9,8 @@ fzf-git-branch-checkout() {
     BRANCH=`echo "$BRANCHES" | fzf -d $(( 2 + $(wc -l <<< "$BRANCHES") )) +m`
     if [ -n "$BRANCH" ]; then
         git checkout $(echo "$BRANCH" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-        zle accept-line
     fi
+    zle accept-line
 }
 zle -N fzf-git-branch-checkout
 
@@ -19,7 +19,11 @@ zle -N fzf-git-branch-checkout
 function fzf-cmd-history() {
     local HISTORY=`history -n 1 | tail -r  | awk '!a[$0]++' | fzf +m`
     BUFFER=$HISTORY
-    CURSOR=$#BUFFER
+    if [ -n "$BRANCH" ]; then
+        CURSOR=$#BUFFER
+    else
+        zle accept-line
+    fi
 }
 zle -N fzf-cmd-history
 
@@ -29,26 +33,26 @@ function fzf-ghq=repository() {
     local REPO=`ghq list -p | fzf +m`
     if [ -n "$REPO" ]; then
         BUFFER="cd $REPO"
-        zle accept-line
     fi
+    zle accept-line
 }
 zle -N fzf-ghq=repository
 
 
 # ssh selected host
 function fzf-ssh-host() {
-  local SSH_HOST=$(awk '
-    tolower($1)=="host" {
-        for (i=2; i<=NF; i++) {
-            if ($i !~ "[*?]") {
-                print $i
+    local SSH_HOST=$(awk '
+        tolower($1)=="host" {
+            for (i=2; i<=NF; i++) {
+                if ($i !~ "[*?]") {
+                    print $i
+                }
             }
         }
-    }
-  ' ~/.ssh/config | sort | fzf +m)
-  if [ -n "$SSH_HOST" ]; then
-    BUFFER="ssh $SSH_HOST"
+    ' ~/.ssh/config | sort | fzf +m)
+    if [ -n "$SSH_HOST" ]; then
+        BUFFER="ssh $SSH_HOST"
+    fi
     zle accept-line
-  fi
 }
 zle -N fzf-ssh-host
