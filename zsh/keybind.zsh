5d8d183afb051d7f5dccb6512ec7c6202f4e43af
lighttiger <lighttiger2505@gmail.com>
2017-09-21 01:17:58 +0900

    Add oneline editor keybind

--- zsh/keybind.zsh
+++ zsh/keybind.zsh
@@ -19,3 +19,14 @@ bindkey '^B' fzf-git-branch-checkout
 bindkey '^R' fzf-cmd-history
 bindkey '^G' fzf-ghq=repository
 bindkey '^\' fzf-ssh-host
+
+# Vim normal mode keybind
+bindkey -M vicmd 'H'  beginning-of-line
+bindkey -M vicmd 'L'  end-of-line
+
+# Vim insert mode keybind
+bindkey -M viins '^A'  beginning-of-line
+bindkey -M viins '^B'  backward-char
+bindkey -M viins '^E'  end-of-line
+bindkey -M viins '^F'  forward-char
+bindkey -M viins '^H'  backward-delete-char
