e9e3bea9c81c82d6ad10f4b16d1c72c4957269c9
Toshikazu Ohashi <toshikazu.ohashi@iridge.jp>
2017-10-16 10:49:30 +0900

    Fix diary cmd

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -27,13 +27,6 @@ alias zb="time ( zsh -i -c exit; )"
 
 alias al="alacritty"
 
-# Launch markdown diary
-function open_diary() {
-  mkdir -p ~/diary/$(date "+%Y/%m")
-  vim ~/diary/$(date "+%Y/%m/%d.md")
-}
-alias dia=open_diary
-
 # Vim shotchut
 alias v=vim
 
@@ -50,3 +43,10 @@ if type nvim > /dev/null 2>&1; then
 else
     alias nvim=vim
 fi
+
+# Launch markdown diary
+function open_diary() {
+  mkdir -p ~/diary/$(date "+%Y/%m")
+  vim ~/diary/$(date "+%Y/%m/%d.md")
+}
+alias dia=open_diary
