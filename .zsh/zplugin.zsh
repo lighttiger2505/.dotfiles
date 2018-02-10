82247042456c3e90c37ceccc8439f385e2102b2a
lighttiger <lighttiger2505@gmail.com>
2018-02-11 04:26:02 +0900

    Add zsh plugins

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -2,11 +2,17 @@ source $HOME/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-zplugin load zsh-users/zsh-autosuggestions
-zplugin load zsh-users/zsh-syntax-highlighting
-zplugin load zsh-users/zsh-completions
-zplugin load b4b4r07/enhancd
-zplugin load greymd/tmux-xpanes
+zplugin light zsh-users/zsh-autosuggestions
+zplugin light zsh-users/zsh-syntax-highlighting
+zplugin light b4b4r07/enhancd
+zplugin light greymd/tmux-xpanes
+zplugin light mollifier/cd-gitroot
+zplugin light b4b4r07/emoji-cli
 zplugin light b4b4r07/zsh-vimode-visual
 zplugin light hchbaw/opp.zsh
+
+zplugin light zsh-users/zsh-completions
+zplugin light felixr/docker-zsh-completion
+
 zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
+zplugin ice from"gh-r" as"program"; zplugin load motemen/ghq
