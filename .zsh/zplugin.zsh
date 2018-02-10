fb6a3ff906567b64571d0ace7ba9f18e43a35591
lighttiger <lighttiger2505@gmail.com>
2018-02-11 05:26:57 +0900

    Clean zplugin install setting

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -8,8 +8,9 @@ source $HOME/.zplugin/bin/zplugin.zsh
 autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zplugin]=_zplugin
 
-zplugin light zsh-users/zsh-autosuggestions
+# Load zsh plugins
 zplugin light zsh-users/zsh-syntax-highlighting
+zplugin light zsh-users/zsh-autosuggestions
 zplugin light b4b4r07/enhancd
 zplugin light greymd/tmux-xpanes
 zplugin light mollifier/cd-gitroot
@@ -17,8 +18,10 @@ zplugin light b4b4r07/emoji-cli
 zplugin light b4b4r07/zsh-vimode-visual
 zplugin light hchbaw/opp.zsh
 
+# Load completions
 zplugin light zsh-users/zsh-completions
 zplugin light felixr/docker-zsh-completion
 
+# Load commands
 zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
 zplugin ice from"gh-r" as"program"; zplugin load motemen/ghq
