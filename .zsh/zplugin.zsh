eecc7aaa73e99d6d89e695d0cca54e667d0a8ecf
Toshikazu Ohashi <toshikazu.ohashi@iridge.jp>
2018-02-14 21:03:20 +0900

    Add zplugin lazy settings

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -11,16 +11,18 @@ autoload -Uz _zplugin
 # Load zsh plugins
 zplugin light zsh-users/zsh-syntax-highlighting
 zplugin light zsh-users/zsh-autosuggestions
-zplugin light b4b4r07/enhancd
-zplugin light greymd/tmux-xpanes
-zplugin light mollifier/cd-gitroot
-zplugin light b4b4r07/emoji-cli
-zplugin light b4b4r07/zsh-vimode-visual
-zplugin light hchbaw/opp.zsh
+
+# Lazy load zsh plugins
+zplugin ice wait'!1'; zplugin light b4b4r07/enhancd
+zplugin ice wait'!1'; zplugin light greymd/tmux-xpanes
+zplugin ice wait'!1'; zplugin light mollifier/cd-gitroot
+zplugin ice wait'!1'; zplugin light b4b4r07/emoji-cli
+zplugin ice wait'!1'; zplugin light b4b4r07/zsh-vimode-visual
+zplugin ice wait'!1'; zplugin light hchbaw/opp.zsh
 
 # Load completions
-zplugin light zsh-users/zsh-completions
-zplugin light felixr/docker-zsh-completion
+zplugin ice wait'!1'; zplugin light zsh-users/zsh-completions
+zplugin ice wait'!1'; zplugin light felixr/docker-zsh-completion
 
 # Load commands
 zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
