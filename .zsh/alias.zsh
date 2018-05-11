4c28c49f633cd4d97600a4e308b6b64f8154d6ae
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-05-11 19:21:09 +0900

    Update aws cli alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -119,10 +119,13 @@ alias -g B='`git branch --all | grep -v HEAD | fzf -m`'
 #####################################################################
 # awscli
 #####################################################################
-# aws profile select
-alias awsprof='export AWS_DEFAULT_PROFILE=`cat ~/.aws/credentials | grep -e "\[\(.*\)\]" | sed -e "s/\[//g" | sed -e "s/\]//g" | sort | fzf`'
-# aws ec2 ip list
-alias ec2='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | select(.Tags!=null) | [.InstanceId, .PublicIpAddress, .PrivateIpAddress, [.Tags[] | select(.Key == \"Name\").Value][]]  | @tsv " | sort -k3'
+if type aws > /dev/null 2>&1; then
+    # aws profile select
+    alias awsprof='export AWS_DEFAULT_PROFILE=`cat ~/.aws/credentials | grep -e "\[\(.*\)\]" | sed -e "s/\[//g" | sed -e "s/\]//g" | sort | fzf`'
+    # aws ec2 ip list
+    alias ec2='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | select(.Tags!=null) | [.InstanceId, .PublicIpAddress, .PrivateIpAddress, [.Tags[] | select(.Key == \"Name\").Value][]] | @tsv " | sort -k3'
+    alias ec2desc='aws ec2 describe-instances | jq -r ".Reservations[].Instances[] | select(.InstanceId==\"i-017b9946a248a7679\")"'
+fi
 
 #####################################################################
 # todoist
