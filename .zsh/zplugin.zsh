44b131f5ca7155ea44e9bf3123288196841f9aa5
lighttiger2505 <lighttiger2505@gmail.com>
2019-05-10 02:05:48 +0900

    Remove unused zsh plugins

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -15,15 +15,8 @@ zplugin light b4b4r07/enhancd
 zplugin light jocelynmallon/zshmarks
 
 # Lazy load zsh plugins
-zplugin ice wait'!1'; zplugin light greymd/tmux-xpanes
-zplugin ice wait'!1'; zplugin light mollifier/cd-gitroot
 zplugin ice wait'!1'; zplugin light b4b4r07/zsh-vimode-visual
-zplugin ice wait'!1'; zplugin light hchbaw/opp.zsh
 
 # Load completions
 zplugin ice blockf; zplugin light zsh-users/zsh-completions
 zplugin ice blockf; zplugin light felixr/docker-zsh-completion
-
-# Load commands
-zplugin ice from"gh-r" as"program"; zplugin load junegunn/fzf-bin
-zplugin ice from"gh-r" as"program"; zplugin load motemen/ghq
