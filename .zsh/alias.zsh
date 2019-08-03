4500231a4b840eb8f14f4dbce785e380ca973093
lighttiger2505 <lighttiger2505@gmail.com>
2019-08-03 19:13:56 +0900

    Add less settings

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -40,6 +40,17 @@ vim-fzf-find() {
 }
 alias fv=vim-fzf-find
 
+# less
+export PAGER=less
+export LESS='-g -i -M -R -S -W -z-4 -x4'
+export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
+export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
+export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
+export LESS_TERMCAP_so=$'\E[01;44;33m' # begin reverse video
+export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
+export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
+export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
+
 #####################################################################
 # Replace lagasy command
 #####################################################################
