f8fbeb06d318ea3a9b63a4c8d6416c73d84cacba
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2020-01-29 22:15:34 +0900

    Clean zsh plugins

--- .zsh/zplugin.zsh
+++ .zsh/zplugin.zsh
@@ -9,14 +9,9 @@ autoload -Uz _zplugin
 (( ${+_comps} )) && _comps[zinit]=_zplugin
 
 # Load zsh plugins
-zinit light zsh-users/zsh-syntax-highlighting
+zinit light zdharma/fast-syntax-highlighting
 zinit light zsh-users/zsh-autosuggestions
-# very slow cd
-# zinit light b4b4r07/enhancd
 zinit light jocelynmallon/zshmarks
-
-# Lazy load zsh plugins
-zinit ice wait'!1'; zinit light b4b4r07/zsh-vimode-visual
 
 # Load completions
 zinit ice blockf; zinit light zsh-users/zsh-completions
