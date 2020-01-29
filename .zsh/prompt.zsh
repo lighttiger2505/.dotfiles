2a9587796d58c5bd3aea57b3b66a99feb2bd38dc
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2020-01-29 21:58:04 +0900

    Change zsh prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,7 +1,5 @@
 autoload -Uz VCS_INFO_get_data_git; VCS_INFO_get_data_git 2> /dev/null
 autoload -Uz colors; colors
-autoload -Uz add-zsh-hook
-autoload -Uz terminfo
 
 setopt prompt_subst
 setopt re_match_pcre
@@ -10,37 +8,19 @@ function current-git-branch() {
     echo -n "$(git rev-parse --abbrev-ref=loose HEAD 2> /dev/null)"
 }
 
-terminfo_down_sc=$terminfo[cud1]$terminfo[cuu1]$terminfo[sc]$terminfo[cud1]
-left_down_prompt_preexec() {
-    print -rn -- $terminfo[el]
-}
-add-zsh-hook preexec left_down_prompt_preexec
-
-function zle-keymap-select zle-line-init zle-line-finish
+function zle-line-init zle-line-finish
 {
-    case $KEYMAP in
-        vicmd)
-            vimmode="$fg[white]-- NORMAL --$reset_color"
-            ;;
-        main|viins)
-            vimmode="$fg[blue]-- INSERT --$reset_color"
-            ;;
-        vivis)
-            vimmode="$fg[green]-- VISUAL --$reset_color"
-            ;;
-    esac
-
-    local p_user="[%F{yellow}%n%f]"
-    local p_cdr="%F{cyan}%~%f"
-    local p_vimjob="%F{green}$([[ $(jobs|grep -c vim) != 0 ]] && print "vim:$(jobs|grep -c vim)")%f"
-    local p_branch="{%F{magenta}$(current-git-branch)%f}"
-    local p_mark="%B%(?,%F{green},%F{red})%(!,#,>)%f%b"
-    PROMPT="%{$terminfo_down_sc${vimmode} | ${p_cdr}  $terminfo[rc]%}${p_user}${p_branch}(${p_vimjob}) ${p_mark} "
+    p_cdr="%F{cyan}%~%f"
+    p_vimjob="%F{green}$([[ $(jobs|grep -c vim) != 0 ]] && print "vim:$(jobs|grep -c vim) ")%f"
+    p_branch="%F{magenta}$(current-git-branch)%f"
+    p_user="%F{yellow}%n%f"
+    p_mark="%B%(?,%F{green},%F{red})%(!,#,$)%f%b"
+    PROMPT="(${p_user}) > {${p_cdr}}[${p_branch}]
+${p_vimjob}${p_mark} "
 
     zle reset-prompt
 }
 
 zle -N zle-line-init
 zle -N zle-line-finish
-zle -N zle-keymap-select
 zle -N edit-command-line
