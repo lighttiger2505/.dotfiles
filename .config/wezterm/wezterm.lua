4911e00b466c6d217c2c5a80426cc2e9ccbd843f
lighttiger2505 <lighttiger2505@gmail.com>
2023-01-23 19:57:10 +0900

    fix: Fix wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,7 +1,16 @@
 local wezterm = require('wezterm')
 return {
 	use_ime = true,
-	color_scheme = "Gruvbox Dark",
-	font = wezterm.font("HackGenNerd Console", { weight = "Regular", stretch = "Normal", italic = false }),
-	colors = { compose_cursor = "orange" },
+	color_scheme = "Gruvbox dark, medium (base16)",
+	-- Show font list `wezterm ls-fonts --list-system`
+	font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	-- Reference color schema `https://wezfurlong.org/wezterm/colorschemes/index.html`
+	keys = {
+		-- Turn off the toggle full screen by MacOS
+		{
+			key = 'Enter',
+			mods = 'ALT',
+			action = wezterm.action.DisableDefaultAssignment,
+		},
+	},
 }
