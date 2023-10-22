bbaa2060b73f630f6d21d7a08ebc851fc8a1ea43
lighttiger2505 <lighttiger2505@gmail.com>
2023-10-22 21:41:42 +0900

    perf: Add lazy setting

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -496,7 +496,7 @@ return {
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        event = { "BufReadPost" },
+        event = "VeryLazy",
     },
 
     {
