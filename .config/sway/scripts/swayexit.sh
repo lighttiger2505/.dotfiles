bceb119d94a524d93ca7f05107e598ac19dc7263
lighttiger2505 <lighttiger2505@gmail.com>
2023-10-22 14:30:58 +0900

    feat: Add systemctl menu

---
+++ .config/sway/scripts/swayexit.sh
@@ -0,0 +1,32 @@
+# vim: ft=sh
+#!/bin/sh
+
+# with openrc use loginctl
+[[ $(cat /proc/1/comm) == "systemd" ]] && logind=systemctl || logind=loginctl
+
+case "$1" in
+    lock)
+        swaylock
+        ;;
+    logout)
+        swaymsg exit
+        ;;
+    suspend)
+        $logind suspend
+        ;;
+    hibernate)
+        $logind hibernate
+        ;;
+    reboot)
+        $logind reboot
+        ;;
+    shutdown)
+        $logind poweroff
+        ;;
+    *)
+        echo "== ! i3exit: missing or invalid argument ! =="
+        echo "Try again with: lock | logout | switch_user | suspend | hibernate | reboot | shutdown"
+        exit 2
+esac
+
+exit 0
