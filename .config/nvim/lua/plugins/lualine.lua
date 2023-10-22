202329d5ee4611ed16cbec0ad888f6ea40ab05a7
lighttiger2505 <lighttiger2505@gmail.com>
2023-10-22 15:30:11 +0900

    feat: Update lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,26 +1,26 @@
-require('lualine').setup {
+require("lualine").setup {
     options = {
         icons_enabled = true,
-        theme = 'gruvbox',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
+        theme = "nightfox",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
         disabled_filetypes = {},
         always_divide_middle = true,
         globalstatus = false,
     },
     sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'filename' },
-        lualine_c = { 'diff', 'diagnostics' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        lualine_a = { "mode" },
+        lualine_b = { "filename" },
+        lualine_c = { "diff", "diagnostics" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { 'filename' },
-        lualine_x = { 'location' },
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
         lualine_y = {},
         lualine_z = {}
     },
