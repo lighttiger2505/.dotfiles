1be3906f4df5fb9fc273c3b274eaa2b58c9b4bda
lighttiger2505 <lighttiger2505@gmail.com>
2023-10-22 15:55:16 +0900

    feat: Update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,35 +1,73 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 return {
 	use_ime = true,
 	enable_wayland = true,
 	color_scheme = "Gruvbox dark, medium (base16)",
 	-- Show font list `wezterm ls-fonts --list-system`
-	font = wezterm.font("HackGen Console NF", { weight = "Regular", stretch = "Normal", style = "Normal" }),
+	font = wezterm.font("UDEV Gothic NF", { weight = "Regular", stretch = "Normal", style = "Normal" }),
 	-- Reference color schema `https://wezfurlong.org/wezterm/colorschemes/index.html`
 	keys = {
 		-- Turn off the toggle full screen by MacOS
 		{
-			key = 'Enter',
-			mods = 'ALT',
+			key = "Enter",
+			mods = "ALT",
 			action = wezterm.action.DisableDefaultAssignment,
 		},
 		-- Turn off the new tab by MacOS
 		{
-			key = 't',
-			mods = 'CMD',
+			key = "t",
+			mods = "CMD",
 			action = wezterm.action.DisableDefaultAssignment,
 		},
 		-- Turn off the new window by MacOS
 		{
-			key = 'n',
-			mods = 'CMD',
+			key = "n",
+			mods = "CMD",
 			action = wezterm.action.DisableDefaultAssignment,
 		},
-		-- paste from the primary selection
+		-- paste from the primary selection by CTRL-V
 		{
-			key = 'V',
-			mods = 'CTRL',
-			action = wezterm.action.PasteFrom 'Clipboard',
+			key = "V",
+			mods = "CTRL",
+			action = wezterm.action.PasteFrom "Clipboard",
+		},
+		{
+			key = "V",
+			mods = "CTRL",
+			action = wezterm.action.PasteFrom "PrimarySelection",
+		},
+		-- paste from the primary selection by CTRL-v
+		{
+			key = "v",
+			mods = "CTRL",
+			action = wezterm.action.PasteFrom "Clipboard",
+		},
+		{
+			key = "v",
+			mods = "CTRL",
+			action = wezterm.action.PasteFrom "PrimarySelection",
+		},
+		-- paste from the primary selection by WINDOW(Meta)-V
+		{
+			key = "V",
+			mods = "CMD",
+			action = wezterm.action.PasteFrom "Clipboard",
+		},
+		{
+			key = "V",
+			mods = "CMD",
+			action = wezterm.action.PasteFrom "PrimarySelection",
+		},
+		-- paste from the primary selection by WINDOW(Meta)-v
+		{
+			key = "v",
+			mods = "CMD",
+			action = wezterm.action.PasteFrom "Clipboard",
+		},
+		{
+			key = "v",
+			mods = "CMD",
+			action = wezterm.action.PasteFrom "PrimarySelection",
 		},
 	},
 }
