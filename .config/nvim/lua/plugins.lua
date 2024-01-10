d6e4cc24991a162e5b69f0d0d06ef76c4ad1d3f2
lighttiger2505 <lighttiger2505@gmail.com>
2024-01-10 09:23:01 +0900

    feat: Add plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -404,6 +404,13 @@ return {
 
     -- Markdown edit
     {
+        "ixru/nvim-markdown",
+        ft = { "markdown" },
+        config = function ()
+            vim.g.vim_markdown_no_default_key_mappings = 1
+        end
+    },
+    {
         "dhruvasagar/vim-table-mode",
         ft = { "markdown" },
     },
