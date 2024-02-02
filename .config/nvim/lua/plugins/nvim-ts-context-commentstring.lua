d7766dde439b06355b0839401b3e4651c5c090b0
lighttiger2505 <lighttiger2505@gmail.com>
2024-02-02 16:38:09 +0900

    fix: Fix plugin setting

--- .config/nvim/lua/plugins/nvim-ts-context-commentstring.lua
+++ .config/nvim/lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,6 +1,4 @@
-require 'nvim-treesitter.configs'.setup {
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    }
+vim.g.skip_ts_context_commentstring_module = true
+require('ts_context_commentstring').setup {
+  enable_autocmd = false,
 }
