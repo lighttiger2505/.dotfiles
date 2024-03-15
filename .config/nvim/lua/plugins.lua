25c796be87d5c5cb5e1fb694f98b3641250b7561
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-15 09:10:10 +0900

    feat: Add plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -562,5 +562,36 @@ return {
             { "y", mode = "n" },
             { "p", mode = "n" },
         },
-    }
+    },
+
+    {
+        "b0o/incline.nvim",
+        event = "VeryLazy",
+        config = function()
+            local helpers = require 'incline.helpers'
+            local devicons = require 'nvim-web-devicons'
+            require('incline').setup {
+                window = {
+                    padding = 0,
+                    margin = { horizontal = 0 },
+                },
+                render = function(props)
+                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+                    if filename == '' then
+                        filename = '[No Name]'
+                    end
+                    local ft_icon, ft_color = devicons.get_icon_color(filename)
+                    local modified = vim.bo[props.buf].modified
+                    return {
+                        ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or
+                        '',
+                        ' ',
+                        { filename, gui = modified and 'bold,italic' or 'bold' },
+                        ' ',
+                        guibg = '#44406e',
+                    }
+                end,
+            }
+        end,
+    },
 }
