4895b13600fd46a5634419e19232bfe86516f730
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-23 23:25:09 +0900

    feat: Add copilot chat

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -612,6 +612,16 @@ return {
     },
 
     {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            { "zbirenbaum/copilot.lua" },
+            { "nvim-lua/plenary.nvim" },
+        },
+        opts = {},
+    },
+
+    {
         "gbprod/yanky.nvim",
         event = "VeryLazy",
         dependencies = {
