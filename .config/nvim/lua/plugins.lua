b63104ac153da086983a139378ff8234bcd7d6f3
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-23 23:22:19 +0900

    feat: Add whichkey nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -768,4 +768,23 @@ return {
             require("scrollbar.handlers.gitsigns").setup()
         end,
     },
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            window = {
+                border = "single",
+                position = "bottom",
+                margin = { 1, 0, 1, 0 },
+                padding = { 1, 2, 1, 2 },
+                winblend = 0,
+                zindex = 1000,
+            },
+        }
+    }
 }
