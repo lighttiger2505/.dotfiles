6a1faaae7a4d39fb49806743849bcbdb497d1aaf
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-24 10:35:53 +0900

    refactor: Move lazy setting

---
+++ .config/nvim/lua/config/lazy.lua
@@ -0,0 +1,33 @@
+local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = require("plugins")
+require("lazy").setup(plugins, {
+    defaults = { lazy = true },
+    performance = {
+        cache = { enabled = true },
+        rtp = {
+            disabled_plugins = {
+                "gzip",
+                "matchit",
+                "matchparen",
+                "netrwPlugin",
+                "rplugin",
+                "tarPlugin",
+                "tohtml",
+                "tutor",
+                "zipPlugin",
+            },
+        },
+    },
+})
