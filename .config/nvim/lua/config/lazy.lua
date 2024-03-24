a18bf92366ac7358d8906340eba8ebed928ad323
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-24 10:46:51 +0900

    refactor: Move plugins dir

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,19 +1,21 @@
-local lazypath = vim.fn.stdpath("data").."/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+        "--branch=stable",
         lazypath,
     })
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = require("plugins")
-require("lazy").setup(plugins, {
+require("lazy").setup({
     defaults = { lazy = true },
+    spec = {
+        { import = "plugins" },
+    },
     performance = {
         cache = { enabled = true },
         rtp = {
