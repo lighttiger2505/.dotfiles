b5661e359a04dc8b561901a00603bcf921f6a4da
lighttiger2505 <lighttiger2505@gmail.com>
2024-03-29 22:19:17 +0900

    feat: Add buffer line

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -156,23 +156,67 @@ return {
     },
 
     {
-        "romgrk/barbar.nvim",
-        version = "^1.0.0",
+        'akinsho/bufferline.nvim',
         event = "VeryLazy",
-        dependencies = {
-            "lewis6991/gitsigns.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        init = function()
-            vim.g.barbar_auto_setup = false
+        dependencies = 'nvim-tree/nvim-web-devicons',
+        config = function()
+            require("bufferline").setup({
+                options = {
+                    groups = {
+                        options = {
+                            toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+                        },
+                        -- items = {
+                        --     {
+                        --         name = "Tests", -- Mandatory
+                        --         highlight = { underline = true, sp = "blue" }, -- Optional
+                        --         priority = 2, -- determines where it will appear relative to other groups (Optional)
+                        --         icon = "", -- Optional
+                        --         matcher = function(buf) -- Mandatory
+                        --             return buf.filename:match('%_test') or buf.filename:match('%_spec')
+                        --         end,
+                        --     },
+                        --     {
+                        --         name = "Docs",
+                        --         highlight = { undercurl = true, sp = "green" },
+                        --         auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
+                        --         -- matcher = function(buf)
+                        --         --     return buf.filename:match('%.md') or buf.filename:match('%.txt')
+                        --         -- end,
+                        --         separator = { -- Optional
+                        --             style = require('bufferline.groups').separator.tab
+                        --         },
+                        --     }
+                        -- }
+                    }
+                },
+            })
         end,
-        opts = {},
         keys = {
-            { "<C-j>",    "<Cmd>BufferPrevious<CR>",         mode = "n", desc = "change prev buffer" },
-            { "<C-k>",    "<Cmd>BufferNext<CR>",             mode = "n", desc = "change next buffer" },
-            { "<Space>q", "<Cmd>BufferClose<CR>",            mode = "n", desc = "buffer close" },
-            { "<Space>j", "<Cmd>BufferPick<CR>",             mode = "n", desc = "sort buffer by directroy" },
-            { "<Space>k", "<Cmd>BufferOrderByDirectory<CR>", mode = "n", desc = "sort buffer by directroy" },
+            { "<C-j>",    "<Cmd>BufferLineCyclePrev<CR>",       mode = "n", desc = "buffer prev" },
+            { "<C-k>",    "<Cmd>BufferLineCycleNext<CR>",       mode = "n", desc = "buffer next" },
+            { "<Space>j", "<Cmd>BufferLineSortByExtension<CR>", mode = "n", desc = "buffer sort by extension" },
+            { "<Space>k", "<Cmd>BufferLineSortByDirectory<CR>", mode = "n", desc = "buffer sort by directory" },
         },
     },
+    -- {
+    --     "romgrk/barbar.nvim",
+    --     version = "^1.0.0",
+    --     event = "VeryLazy",
+    --     dependencies = {
+    --         "lewis6991/gitsigns.nvim",
+    --         "nvim-tree/nvim-web-devicons",
+    --     },
+    --     init = function()
+    --         vim.g.barbar_auto_setup = false
+    --     end,
+    --     opts = {},
+    --     keys = {
+    --         { "<C-j>",    "<Cmd>BufferPrevious<CR>",         mode = "n", desc = "change prev buffer" },
+    --         { "<C-k>",    "<Cmd>BufferNext<CR>",             mode = "n", desc = "change next buffer" },
+    --         { "<Space>q", "<Cmd>BufferClose<CR>",            mode = "n", desc = "buffer close" },
+    --         { "<Space>j", "<Cmd>BufferPick<CR>",             mode = "n", desc = "sort buffer by directroy" },
+    --         { "<Space>k", "<Cmd>BufferOrderByDirectory<CR>", mode = "n", desc = "sort buffer by directroy" },
+    --     },
+    -- },
 }
