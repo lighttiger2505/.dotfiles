bfda013a7b2810c342e64a356306084862b83e46
lighttiger2505 <lighttiger2505@gmail.com>
2024-04-13 12:46:27 +0900

    feat: Update zsh keybind

--- .zsh/keybind.zsh
+++ .zsh/keybind.zsh
@@ -2,11 +2,8 @@
 bindkey -v
 
 # Search history
-autoload history-search-end
-zle -N history-beginning-search-backward-end history-search-end
-zle -N history-beginning-search-forward-end history-search-end
-bindkey "^P" history-beginning-search-backward-end
-bindkey "^N" history-beginning-search-forward-end
+bindkey '^P' atuin-up-search
+bindkey '^N' atuin-up-search
 
 # Vim normal mode keybind
 bindkey -M vicmd 'H'  beginning-of-line
