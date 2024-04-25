5ba8731e003af253c57a6015b4217872f5e7f6da
lighttiger2505 <lighttiger2505@gmail.com>
2024-04-25 20:21:50 +0900

    feat: Add git linker

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -263,4 +263,14 @@ return {
         config = true,
         event = "VeryLazy",
     },
+
+    {
+        "linrongbin16/gitlinker.nvim",
+        cmd = "GitLink",
+        opts = {},
+        keys = {
+            { "gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
+            { "gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+        },
+    },
 }
