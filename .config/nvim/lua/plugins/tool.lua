3c4abfb61cbf5b4fbdf0a429fb81b9735ca2eb09
lighttiger2505 <lighttiger2505@gmail.com>
2024-04-25 21:15:29 +0900

    refactor: Clean unused plugin

--- .config/nvim/lua/plugins/tool.lua
+++ .config/nvim/lua/plugins/tool.lua
@@ -7,10 +7,7 @@ return {
             local kmopt = { noremap = true, silent = true }
             vim.keymap.set("n", "<Leader>bb", "<Plug>(openbrowser-smart-search)", kmopt)
             vim.keymap.set("x", "<Leader>bb", "<Plug>(openbrowser-smart-search)", kmopt)
-            vim.keymap.set("n", "<Leader>bh", "<Cmd>OpenGithubFile<CR>", kopts)
-            vim.keymap.set("x", "<Leader>bh", ":OpenGithubFile<CR>", kopts)
         end,
-        dependencies = { "tyru/open-browser-github.vim" },
     },
 
     {
