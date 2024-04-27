2aca18de21e31abd06c15119fcc3f9c98f6b388c
lighttiger2505 <lighttiger2505@gmail.com>
2024-04-27 20:57:08 +0900

    feat: Add octo.nvim

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -258,4 +258,17 @@ return {
             { "gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
         },
     },
+
+    {
+        "pwntester/octo.nvim",
+        cmd = "Octo",
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require("octo").setup()
+        end
+    },
 }
