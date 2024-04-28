399a4d39305e9533453f02bd6bfb344d6b277f4c
lighttiger2505 <lighttiger2505@gmail.com>
2024-04-28 19:49:47 +0900

    feat: Modify close buffer

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -187,15 +187,9 @@ return {
     },
 
     {
-        "famiu/bufdelete.nvim",
+        "kazhala/close-buffers.nvim",
         config = function()
-            require('close_buffers').setup({
-                filetype_ignore = {},                            -- Filetype to ignore when running deletions
-                file_glob_ignore = {},                           -- File name glob pattern to ignore when running deletions (e.g. '*.md')
-                file_regex_ignore = {},                          -- File name regex pattern to ignore when running deletions (e.g. '.*[.]md')
-                preserve_window_layout = { 'this', 'nameless' }, -- Types of deletion that should preserve the window layout
-                next_buffer_cme = nil,                           -- Custom function to retrieve the next buffer when preserving window layout
-            })
+            require('close_buffers').setup({})
         end,
         keys = {
             { "<Space>c", function() require('close_buffers').delete({ type = 'hidden', force = true }) end, mode = "n", desc = "Delete all non-visible buffers" },
