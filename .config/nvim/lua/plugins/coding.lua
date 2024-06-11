eb057d29bbc2a60635c432a8c037cc2113e5ac8b
lighttiger2505 <lighttiger2505@gmail.com>
2024-06-11 18:04:27 +0900

    fix: Fix bug

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -57,7 +57,9 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         config = function()
-            require("go").setup()
+            require("go").setup({
+                lsp_codelens = false,
+            })
         end,
         init = function()
             local kmopt = { noremap = true, silent = true }
