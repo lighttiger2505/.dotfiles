638d1f30043ed893be98177d2d6a51e43670cd33
lighttiger2505 <lighttiger2505@gmail.com>
2024-06-23 12:15:56 +0900

    refactor: Remove comment plugin

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,25 +1,9 @@
 return {
     {
-        "numToStr/Comment.nvim",
-        dependencies = {
-            {
-                "JoosepAlviste/nvim-ts-context-commentstring",
-                config = function()
-                    require("ts_context_commentstring").setup({
-                        enable_autocmd = false,
-                    })
-                end,
-            },
-        },
-        config = function()
-            require("Comment").setup({
-                pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-            })
-        end,
-        keys = {
-            { "gc", mode = "n" },
-            { "gc", mode = "v" },
-        },
+        "folke/ts-comments.nvim",
+        opts = {},
+        event = "VeryLazy",
+        enabled = vim.fn.has("nvim-0.10.0") == 1,
     },
 
     {
