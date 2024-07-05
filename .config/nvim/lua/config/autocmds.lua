261f40baad858b05b14a1c3644f5be55dc9a0fe7
lighttiger2505 <lighttiger2505@gmail.com>
2024-07-05 13:44:49 +0900

    feat: Modify markdown setting

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,59 +6,59 @@ vim.api.nvim_create_augroup(group_name, { clear = true })
 
 -- envrc filetype
 autocmd({ 'BufRead', 'BufNewFile' }, {
-  group = group_name,
-  pattern = { ".envrc" },
-  callback = function()
-    l.filetype = 'sh'
-  end,
+    group = group_name,
+    pattern = { ".envrc" },
+    callback = function()
+        l.filetype = 'sh'
+    end,
 })
 
 -- Disable markdown collapse
 autocmd('FileType', {
-  group = group_name,
-  pattern = { "markdown" },
-  callback = function()
-    l.conceallevel = 3
-    l.concealcursor = "c"
-  end,
+    group = group_name,
+    pattern = { "markdown" },
+    callback = function()
+        l.conceallevel = 2
+        l.concealcursor = "c"
+    end,
 })
 
 -- Spell check gitcommit
 autocmd('FileType', {
-  group = group_name,
-  pattern = { 'gitcommit', 'NeogitCommitMessage' },
-  callback = function()
-    l.spell = true
-  end,
+    group = group_name,
+    pattern = { 'gitcommit', 'NeogitCommitMessage' },
+    callback = function()
+        l.spell = true
+    end,
 })
 
 -- Save last cursor position
 autocmd('BufRead', {
-  group = group_name,
-  pattern = { '*' },
-  command = [[if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g`\"" | endif]],
+    group = group_name,
+    pattern = { '*' },
+    command = [[if line("'\"") > 0 && line("'\"") <= line("$") | exe "normal g`\"" | endif]],
 })
 
 -- Show quickfix after grepcmd
 autocmd('QuickFixCmdPost', {
-  group = group_name,
-  pattern = { 'vim', 'grep', 'make' },
-  command = [[if len(getqflist()) != 0 | cwindow | endif]],
+    group = group_name,
+    pattern = { 'vim', 'grep', 'make' },
+    command = [[if len(getqflist()) != 0 | cwindow | endif]],
 })
 
 -- Tarminal buffer guitter
 autocmd('TermOpen', {
-  group = group_name,
-  callback = function()
-    l.relativenumber = false
-    l.number = false
-  end,
+    group = group_name,
+    callback = function()
+        l.relativenumber = false
+        l.number = false
+    end,
 })
 
 -- Tarminal buffer guitter
 autocmd('TextYankPost', {
-  group = group_name,
-  callback = function()
-    vim.highlight.on_yank { higroup = "IncSearch", timeout = 200 }
-  end,
+    group = group_name,
+    callback = function()
+        vim.highlight.on_yank { higroup = "IncSearch", timeout = 200 }
+    end,
 })
--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -48,20 +48,13 @@ return {
         name = 'render-markdown',
         config = function()
             require('render-markdown').setup({
-                -- Window options to use that change between rendered and raw view
                 win_options = {
-                    -- See :h 'conceallevel'
                     conceallevel = {
-                        -- Used when not being rendered, get user setting
                         default = vim.api.nvim_get_option_value('conceallevel', {}),
-                        -- Used when being rendered, concealed text is completely hidden
-                        rendered = 3,
+                        rendered = 2,
                     },
-                    -- See :h 'concealcursor'
                     concealcursor = {
-                        -- Used when not being rendered, get user setting
                         default = vim.api.nvim_get_option_value('concealcursor', {}),
-                        -- Used when being rendered, conceal text in all modes
                         rendered = 'c',
                     },
                 },
