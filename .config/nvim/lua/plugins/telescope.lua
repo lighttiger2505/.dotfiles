08ecc2dae5bebb7b32a24369c199267900f5e14f
lighttiger2505 <lighttiger2505@gmail.com>
2024-07-26 20:43:28 +0900

    style(telescope): format telescope-fzf-native.nvim build command
    The build command for telescope-fzf-native.nvim in the telescope
    plugin configuration has been formatted for better readability. No
    functional changes were made.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -29,7 +29,11 @@ return {
         "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build =
+                'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+            },
             "cbochs/grapple.nvim",
         },
         config = function()
@@ -72,5 +76,4 @@ return {
             telescope.load_extension("grapple")
         end,
     },
-
 }
