ae77ada3837411193e7dcb787d0a8602610c7dec
lighttiger2505 <lighttiger2505@gmail.com>
2024-08-29 22:20:25 +0900

    feat(alias): add author filter to 'hfp' alias
    The 'hfp' alias now includes an author filter to show only pull requests
    created by the current user. This change enhances the usability by
    narrowing down the search results to the user's own pull requests.

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -307,7 +307,7 @@ alias hb='gh repo view --web'
 alias hp='gh pr view --web'
 alias hci='gh workflow view --web'
 alias hcp='gh pr create --template "pull_request_template.md"'
-alias hfp='gh fzf pr'
+alias hfp='gh fzf pr --author @me'
 
 alias hpr=github-pr-review
 function github-pr-review() {
