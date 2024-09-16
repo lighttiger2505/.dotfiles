ce82aa397e860c69abcebe8c8bf73d640bafeec4
lighttiger2505 <lighttiger2505@gmail.com>
2024-09-16 15:38:39 +0900

    feat: Add markdown preview plugin

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -83,4 +83,14 @@ return {
             },
         },
     },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 }
