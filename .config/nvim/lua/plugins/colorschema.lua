30f29e9561dae7fdfb80cc93a6ba03d8b731d10a
lighttiger2505 <lighttiger2505@gmail.com>
2024-10-26 12:14:14 +0900

    feat(colors): update colorscheme configuration
    - Switched from 'kanagawa' to 'nightfox' as the primary colorscheme
    - Added 'lazy' and 'priority' properties to 'tokyonight', 'gruvbox', and
      'rose-pine' plugins for consistent loading behavior

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -1,30 +1,32 @@
 return {
     {
-        "rebelot/kanagawa.nvim",
+        "EdenEast/nightfox.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd([[colorscheme kanagawa]])
+            vim.cmd([[colorscheme nightfox]])
             vim.cmd([[highlight WinSeparator guifg=#928374]])
         end,
     },
     {
-        "EdenEast/nightfox.nvim",
+        "rebelot/kanagawa.nvim",
         lazy = false,
         priority = 1000,
-        -- config = function()
-        --     vim.cmd([[colorscheme nightfox]])
-        --     vim.cmd([[highlight WinSeparator guifg=#928374]])
-        -- end,
     },
     {
         "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
     },
     {
         "ellisonleao/gruvbox.nvim",
+        lazy = false,
+        priority = 1000,
     },
     {
         "rose-pine/neovim",
         name = "rose-pine",
+        lazy = false,
+        priority = 1000,
     },
 }
