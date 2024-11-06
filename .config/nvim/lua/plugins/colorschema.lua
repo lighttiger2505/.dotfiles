63135f04bce70e2524d8f3c6766255b4ef3a65df
lighttiger2505 <lighttiger2505@gmail.com>
2024-11-06 09:39:47 +0900

    feat(colorschema): add catppuccin theme
    Added the catppuccin theme to the Neovim configuration. This theme is
    set to load lazily with a high priority, ensuring it is available
    immediately upon startup.

--- .config/nvim/lua/plugins/colorschema.lua
+++ .config/nvim/lua/plugins/colorschema.lua
@@ -29,4 +29,10 @@ return {
         lazy = false,
         priority = 1000,
     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+    }
 }
