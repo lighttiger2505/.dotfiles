80e2f800c54ad019280085e2cb05664a30b302e1
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-09 15:26:05 +0900

    refactor: comment out ssh-agent setup

--- .zsh/sync.zsh
+++ .zsh/sync.zsh
@@ -7,15 +7,15 @@ source $HOME/.zsh/completion.zsh
 source $HOME/.zsh/fzf.zsh
 source $HOME/.zsh/keybind.zsh
 
-# ssh-agent
-if [ "$(pgrep ssh-agent 2> /dev/null)" = "" ]; then
-    eval $(ssh-agent) > /dev/null
-    case ${OSTYPE} in
-        darwin*)
-            ssh-add --apple-use-keychain ~/.ssh/id_rsa > /dev/null 2>&1
-            ;;
-        linux-gnu*)
-            ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
-            ;;
-    esac
-fi
+# # ssh-agent
+# if [ "$(pgrep ssh-agent 2> /dev/null)" = "" ]; then
+#     eval $(ssh-agent) > /dev/null
+#     case ${OSTYPE} in
+#         darwin*)
+#             ssh-add --apple-use-keychain ~/.ssh/id_rsa > /dev/null 2>&1
+#             ;;
+#         # linux-gnu*)
+#         #     ssh-add ~/.ssh/id_rsa > /dev/null 2>&1
+#         #     ;;
+#     esac
+# fi
