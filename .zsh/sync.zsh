d753becfbdabf55d6ccba44cb0c43d60aa141329
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-09 12:42:22 +0900

    feat: Remove auto start tmux

--- .zsh/sync.zsh
+++ .zsh/sync.zsh
@@ -19,22 +19,3 @@ if [ "$(pgrep ssh-agent 2> /dev/null)" = "" ]; then
             ;;
     esac
 fi
-
-# auto start tmux
-if [[ ! -n $TMUX && $- == *l* ]]; then
-    # get the IDs
-    ID="`tmux list-sessions`"
-    if [[ -z "$ID" ]]; then
-        tmux new-session
-    fi
-    create_new_session="Create New Session"
-    ID="$ID\n${create_new_session}:"
-    ID="`echo $ID | $PERCOL | cut -d: -f1`"
-    if [[ "$ID" = "${create_new_session}" ]]; then
-        tmux new-session
-    elif [[ -n "$ID" ]]; then
-        tmux attach-session -t "$ID"
-    else
-        :  # Start terminal normally
-    fi
-fi
