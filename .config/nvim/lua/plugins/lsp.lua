097a4f6fb1435265187e9891069dd0f332894cc3
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-11 10:38:05 +0900

    fix(lint): ignore errors during linting on save
    Updated the linting configuration to ignore errors during the linting
    process when a buffer is saved. This change ensures that linting does
    not interrupt the workflow due to minor issues.
    
    - Modified the callback function in lsp.lua to include ignore_errors
      parameter.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -228,7 +228,7 @@ return {
             }
             vim.api.nvim_create_autocmd({ "BufWritePost" }, {
                 callback = function()
-                    require("lint").try_lint()
+                    require("lint").try_lint(nil, { ignore_errors = true })
                 end,
             })
         end,
