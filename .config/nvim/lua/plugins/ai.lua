5b4922b5c125b8345a52b585a296e085a9b2343e
lighttiger2505 <lighttiger2505@gmail.com>
2025-02-23 21:40:29 +0900

    feat: add avante.nvim plugin with dependencies
    Added the avante.nvim plugin to the Neovim configuration with its
    dependencies. Configured the plugin to use the Copilot provider and
    set various options for OpenAI API integration. Included additional
    plugins for enhanced functionality.

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -196,4 +196,52 @@ return {
             },
         },
     },
+
+    {
+        "yetone/avante.nvim",
+        event = "VeryLazy",
+        lazy = false,
+        version = false,
+        opts = {
+            provider = "copilot",
+            openai = {
+                endpoint = "https://api.openai.com/v1",
+                model = "gpt-4o",
+                timeout = 30000,
+                temperature = 0,
+                max_tokens = 4096,
+            },
+        },
+        build = "make",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-telescope/telescope.nvim",
+            "hrsh7th/nvim-cmp",
+            "zbirenbaum/copilot.lua",
+            {
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        },
+    }
 }
