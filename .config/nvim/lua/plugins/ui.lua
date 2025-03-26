963315ff9ca4448ad78a7ec6cccb836ee823532f
lighttiger2505 <lighttiger2505@gmail.com>
2025-03-26 09:22:02 +0900

    style: adjust smear-cursor animation parameters

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -244,9 +244,10 @@ return {
         "sphamba/smear-cursor.nvim",
         event = "VeryLazy",
         opts = {
-            -- stiffness = 0.8,
-            -- trailing_stiffness = 0.5,
-            -- distance_stop_animating = 0.5,
+            stiffness = 0.7,
+            trailing_stiffness = 0.4,
+            distance_stop_animating = 0.5,
+            smear_to_cmd = false,
         },
     },
 }
