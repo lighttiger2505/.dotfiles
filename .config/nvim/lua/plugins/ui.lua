f6d3e3d09e6e56b391595bb6a63d65f86d70b821
lighttiger2505 <lighttiger2505@gmail.com>
2025-03-29 11:13:23 +0900

    chore: enable smear animation to command line
    Enable smear_to_cmd in mini.animate plugin configuration to extend the
    animation effect to command line operations, providing a more consistent
    visual experience throughout the interface.

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -247,7 +247,7 @@ return {
             stiffness = 0.7,
             trailing_stiffness = 0.4,
             distance_stop_animating = 0.5,
-            smear_to_cmd = false,
+            smear_to_cmd = true,
         },
     },
 }
