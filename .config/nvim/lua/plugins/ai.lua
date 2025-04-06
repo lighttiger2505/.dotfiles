62bb40236073f0513341542cefb009981e458417
lighttiger2505 <lighttiger2505@gmail.com>
2025-04-06 14:56:44 +0900

    refactor: remove gp.nvim ChatGPT plugin

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -59,48 +59,6 @@ return {
     },
 
     {
-        "robitx/gp.nvim",
-        event = "VeryLazy",
-        config = function ()
-            local conf = {
-                chat_shortcut_respond = { modes = { "n", "i", "v", "x" }, shortcut = "<C-j><C-j>" },
-                chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<C-j>d" },
-                chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<C-j>s" },
-                chat_shortcut_new = { modes = { "n", "i", "v", "x" }, shortcut = "<C-j>c" },
-                openai_api_key = { "op", "read", "op://Personal/OpenAI/credential", "--no-newline" },
-                hooks = {
-                    Translator = function (gp, params)
-                        local chat_system_prompt =
-                        "You are a skilled translator with a deep understanding of both English and Japanese. Please translate the following English text into natural, fluent Japanese while preserving the context, nuance, and style of the original."
-                        gp.cmd.ChatNew(params, chat_system_prompt)
-                    end,
-                },
-            }
-            require("gp").setup(conf)
-        end,
-        keys = {
-            {
-                "<Leader>cc",
-                "<Cmd>GpChatToggle<CR>",
-                mode = { "n", "x" },
-                desc = "ChatGPT Toggle chat window",
-            },
-            {
-                "<Leader>cn",
-                "<Cmd>GpChatNew<CR>",
-                mode = { "n", "x" },
-                desc = "ChatGPT Create new chat window",
-            },
-            {
-                "<Leader>ct",
-                "<Cmd>GpTranslator vsplit<CR>",
-                mode = { "n", "x" },
-                desc = "ChatGPT call translate agent",
-            },
-        },
-    },
-
-    {
         "yetone/avante.nvim",
         event = "VeryLazy",
         version = false,
