54254dad51df53afafc4430642dacac8fae4968d
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-05 16:54:12 +0900

    feat(nvim): add lazydev for Lua development support

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -227,4 +227,14 @@ return {
             })
         end,
     },
+
+    {
+        "folke/lazydev.nvim",
+        ft = "lua",
+        opts = {
+            library = {
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 }
