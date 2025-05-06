25a95ce021fa18fa4b18d3c6400d733e8f444503
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-06 19:56:40 +0900

    feat(ui): set lualine theme to auto-detect

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -39,7 +39,7 @@ return {
             require("lualine").setup({
                 options = {
                     icons_enabled = true,
-                    theme = "nightfox",
+                    theme = "auto",
                     component_separators = { left = "", right = "" },
                     section_separators = { left = "", right = "" },
                     disabled_filetypes = {},
