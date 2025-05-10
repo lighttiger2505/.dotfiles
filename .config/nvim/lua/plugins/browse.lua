85f8421f875fe53445ec3f5a04ef379dcf830837
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-10 20:05:01 +0900

    feat(plugins): add gx.nvim for URL browsing from Neovim

---
+++ .config/nvim/lua/plugins/browse.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "chrishrb/gx.nvim",
+        keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+        cmd = { "Browse" },
+        init = function()
+            vim.g.netrw_nogx = 1 -- disable netrw gx
+        end,
+        dependencies = { "nvim-lua/plenary.nvim" }, -- Required for Neovim < 0.10.0
+        config = true, -- default settings
+    },
+}
