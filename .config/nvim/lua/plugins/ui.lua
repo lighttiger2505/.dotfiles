53a712dde428f766c59af05d31ba2bc6f64c6a67
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-10 13:22:17 +0900

    refactor(plugins): move buffer plugins to separate file

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -183,52 +183,6 @@ return {
     },
 
     {
-        "romgrk/barbar.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "lewis6991/gitsigns.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        init = function()
-            vim.g.barbar_auto_setup = false
-        end,
-        opts = {
-            animation = false,
-        },
-        keys = {
-            { "<C-h>", "<Cmd>BufferPrevious<CR>", mode = "n", desc = "BarBar Move buffer prev" },
-            { "<C-l>", "<Cmd>BufferNext<CR>", mode = "n", desc = "BarBar Move buffer next" },
-            { "<Space>j", "<Cmd>BufferOrderByName<CR>", mode = "n", desc = "BarBar Sort buffer by name" },
-            { "<Space>k", "<Cmd>BufferOrderByDirectory<CR>", mode = "n", desc = "BarBar Sort buffer by directory" },
-        },
-    },
-
-    {
-        "kazhala/close-buffers.nvim",
-        config = function()
-            require("close_buffers").setup({})
-        end,
-        keys = {
-            {
-                "<Space>a",
-                function()
-                    require("close_buffers").delete({ type = "hidden", force = true })
-                end,
-                mode = "n",
-                desc = "CloseBuffers Close all non-visible buffers",
-            },
-            {
-                "<Space>q",
-                function()
-                    require("close_buffers").delete({ type = "this" })
-                end,
-                mode = "n",
-                desc = "CloseBuffers Close the current buffer",
-            },
-        },
-    },
-
-    {
         "shortcuts/no-neck-pain.nvim",
         cmd = "NoNeckPain",
         config = function()
