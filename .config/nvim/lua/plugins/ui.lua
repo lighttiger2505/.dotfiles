a1da56370f07cdf1255d1c8f4c31956f9b8cd8b0
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-10 13:21:58 +0900

    feat(ui): configure which-key to use helix preset

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -175,17 +175,9 @@ return {
         "folke/which-key.nvim",
         event = "VeryLazy",
         opts = {
+            preset = "helix",
             win = {
                 border = "single",
-            },
-        },
-        keys = {
-            {
-                "<leader>?",
-                function()
-                    require("which-key").show({ global = false })
-                end,
-                desc = "Buffer Local Keymaps (which-key)",
             },
         },
     },
