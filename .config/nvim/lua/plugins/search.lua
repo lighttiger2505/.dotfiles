1568c3903ba7dc7066d0e1f031f86793387154d5
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-10 17:40:00 +0900

    feat(nvim): add flash.nvim for enhanced navigation

--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -51,4 +51,29 @@ return {
             },
         },
     },
+
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            {
+                "<Enter>",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function()
+                    require("flash").treesitter()
+                end,
+                desc = "Flash Treesitter",
+            },
+        },
+    },
 }
