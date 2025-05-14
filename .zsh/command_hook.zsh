49c210460d812fc6fb3191df06f87b6cc6440191
lighttiger2505 <lighttiger2505@gmail.com>
2025-05-14 14:30:40 +0900

    feat(zsh): add Google Cloud SDK support to shell config

--- .zsh/command_hook.zsh
+++ .zsh/command_hook.zsh
@@ -16,3 +16,8 @@ fi
 if [ -e $HOME/.local/bin/mise ]; then
     eval "$(~/.local/bin/mise activate zsh)"
 fi
+
+# Google Cloud SDK.
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f $HOME/google-cloud-sdk/path.zsh.inc ]; then . $HOME/google-cloud-sdk/path.zsh.inc; fi
+if [ -f $HOME/google-cloud-sdk/completion.zsh.inc ]; then . $HOME/google-cloud-sdk/completion.zsh.inc; fi
