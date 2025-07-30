3539279000e63b194f4ce02f456bacb782ab1ddf
lighttiger2505 <lighttiger2505@gmail.com>
2025-07-30 21:52:21 +0900

    fix(lsp): use fallback for lsp_format on save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -178,7 +178,7 @@ return {
                 go = { "goimports", "gofmt" },
             },
             format_on_save = {
-                lsp_format = "first",
+                lsp_format = "fallback",
                 timeout_ms = 500,
             },
         },
