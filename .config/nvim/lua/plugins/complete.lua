4df57cb75d423b85e96917b36076ee14d2a775cd
lighttiger2505 <lighttiger2505@gmail.com>
2025-08-02 17:43:16 +0900

    feat(nvim): migrate to blink.cmp from nvim-cmp

--- .config/nvim/lua/plugins/complete.lua
+++ .config/nvim/lua/plugins/complete.lua
@@ -1,214 +1,59 @@
+---@module "lazy"
+---@type LazySpec
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            { "f3fora/cmp-spell" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-cmdline" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-path" },
-            { "lukas-reineke/cmp-rg" },
-            { "onsails/lspkind.nvim" },
-            { "zbirenbaum/copilot-cmp" },
-            { "garymjr/nvim-snippets" },
-            { "hrsh7th/cmp-nvim-lsp-signature-help" },
-        },
-        config = function()
-            local cmp = require("cmp")
-            local lspkind = require("lspkind")
-
-            require("copilot_cmp").setup()
-
-            lspkind.init({
-                mode = "symbol_text",
-                preset = "codicons",
-                symbol_map = {
-                    Text = "󰉿",
-                    Method = "󰆧",
-                    Function = "󰊕",
-                    Constructor = "",
-                    Field = "󰜢",
-                    Variable = "󰀫",
-                    Class = "󰠱",
-                    Interface = "",
-                    Module = "",
-                    Property = "󰜢",
-                    Unit = "󰑭",
-                    Value = "󰎠",
-                    Enum = "",
-                    Keyword = "󰌋",
-                    Snippet = "",
-                    Color = "󰏘",
-                    File = "󰈙",
-                    Reference = "󰈇",
-                    Folder = "󰉋",
-                    EnumMember = "",
-                    Constant = "󰏿",
-                    Struct = "󰙅",
-                    Event = "",
-                    Operator = "󰆕",
-                    TypeParameter = "",
-                    Copilot = "",
-                },
-            })
-
-            cmp.setup({
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                mapping = {
-                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-                    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.mapping.close(),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Replace,
-                        select = true,
-                    }),
-                },
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "snippets" },
-                    { name = "nvim_lsp" },
-                    { name = "copilot" },
-                }, {
-                    { name = "rg" },
-                    { name = "buffer" },
-                }),
-                formatting = {
-                    format = lspkind.cmp_format({
-                        mode = "symbol_text",
-                        maxwidth = 50,
-                        ellipsis_char = "...",
-                        before = function(_, vim_item)
-                            return vim_item
-                        end,
-                        menu = {
-                            buffer = "[Buf]",
-                            rg = "[RG]",
-                            nvim_lsp = "[LSP]",
-                            snippets = "[Snip]",
-                        },
-                    }),
-                },
-            })
-
-            cmp.setup.cmdline("/", {
-                completion = {
-                    completeopt = "menu,menuone,noselect",
-                },
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp_document_symbol" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-            cmp.setup.cmdline(":", {
-                completion = {
-                    completeopt = "menu,menuone,noselect",
-                },
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = "path" },
-                }, {
-                    {
-                        name = "cmdline",
-                        option = {
-                            ignore_cmds = {
-                                "make",
-                                "Make",
-                                "grep",
-                                "Grep",
-                                "Man",
-                                "!",
-                            },
-                        },
-                    },
-                }),
-            })
-
-            -- Set configuration for specific filetype.
-            cmp.setup.filetype("gitcommit", {
-                sources = cmp.config.sources({
-                    { name = "spell" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-            cmp.setup.filetype("markdown", {
-                sources = cmp.config.sources({
-                    { name = "buffer" },
-                }),
-            })
-            cmp.setup.filetype("csv", {
-                sources = cmp.config.sources({
-                    { name = "buffer" },
-                }),
-            })
-            cmp.setup.filetype("codecompanion", {
-                sources = cmp.config.sources({
-                    { name = "codecompanion" },
-                }),
-            })
-        end,
+    "saghen/blink.cmp",
+    dependencies = {
+        "mikavilpas/blink-ripgrep.nvim",
+        "giuxtaposition/blink-cmp-copilot",
     },
-
-    {
-        "garymjr/nvim-snippets",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-        },
-        config = function()
-            require("snippets").setup({ friendly_snippets = true })
-        end,
-        keys = {
-            {
-                "<Tab>",
-                function()
-                    if vim.snippet.active({ direction = 1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(1)
-                        end)
-                        return
-                    end
-                    return "<Tab>"
-                end,
-                expr = true,
-                silent = true,
-                mode = "i",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+        sources = {
+            keymap = { preset = "default" },
+            default = {
+                "copilot",
+                "lsp",
+                "path",
+                "snippets",
+                "buffer",
+                "ripgrep",
             },
-            {
-                "<Tab>",
-                function()
-                    vim.schedule(function()
-                        vim.snippet.jump(1)
-                    end)
-                end,
-                expr = true,
-                silent = true,
-                mode = "s",
-            },
-            {
-                "<S-Tab>",
-                function()
-                    if vim.snippet.active({ direction = -1 }) then
-                        vim.schedule(function()
-                            vim.snippet.jump(-1)
-                        end)
-                        return
-                    end
-                    return "<S-Tab>"
-                end,
-                expr = true,
-                silent = true,
-                mode = { "i", "s" },
+            completion = { documentation = { auto_show = true } },
+            providers = {
+                ripgrep = {
+                    module = "blink-ripgrep",
+                    name = "Ripgrep",
+                    ---@module "blink-ripgrep"
+                    ---@type blink-ripgrep.Options
+                    opts = {},
+                    transform_items = function(_, items)
+                        for _, item in ipairs(items) do
+                            item.labelDetails = {
+                                description = "(rg)",
+                            }
+                        end
+                        return items
+                    end,
+                },
+                copilot = {
+                    name = "copilot",
+                    module = "blink-cmp-copilot",
+                    score_offset = 100,
+                    async = true,
+                    transform_items = function(_, items)
+                        for _, item in ipairs(items) do
+                            item.kind_icon = ""
+                            item.labelDetails = {
+                                description = "(copilot)",
+                            }
+                        end
+                        return items
+                    end,
+                },
             },
         },
+        fuzzy = { implementation = "prefer_rust_with_warning" },
+        opts_extend = { "sources.default" },
     },
 }
