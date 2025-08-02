6b7b9dd7668adee6768bfd3623f95b2bffa1c7d9
lighttiger2505 <lighttiger2505@gmail.com>
2025-08-02 20:14:12 +0900

    feat: Add custom prompt to codecompanion.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -90,6 +90,48 @@ git:staged
                         adapter = "copilot",
                     },
                 },
+                prompt_library = {
+                    ["Commit Message"] = {
+                        strategy = "inline",
+                        description = "Generate a commit message",
+                        opts = {
+                            short_name = "commit_message",
+                            auto_submit = true,
+                            placement = "replace",
+                        },
+                        prompts = {
+                            {
+                                role = "user",
+                                content = function()
+                                    return string.format(
+                                        [=[
+#{buffer}
+
+You are an expert at following the Conventional Commit specification. Given the git diff listed below, please generate a commit message for me:
+
+```diff
+%s
+```
+
+When unsure about the module names to use in the commit message, you can refer to the last 20 commit messages in this repository:
+
+```
+%s
+```
+
+Output only the commit message without any explanations and follow-up suggestions.
+]=],
+                                        vim.fn.system("git diff --no-ext-diff --staged"),
+                                        vim.fn.system('git log --pretty=format:"%s" -n 20')
+                                    )
+                                end,
+                                opts = {
+                                    contains_code = true,
+                                },
+                            },
+                        },
+                    },
+                },
             })
         end,
         keys = {
