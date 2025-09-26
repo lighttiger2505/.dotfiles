5d9c9c9b7f346cfe6b92094e58b1d0444ea667bd
lighttiger2505 <lighttiger2505@gmail.com>
2025-09-26 23:37:25 +0900

    refactor(markdown): restructure obsidian workspaces and add Zettelkasten note ID generation
    Reorganized workspace structure from literature/fleeting/permanent/structured notes to a numbered vault system (Index/Work/Private/Areas/Archive). Added custom note_id_func with timestamp-based Zettelkasten format and configured notes_subdir for better organization.

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -74,25 +74,47 @@ return {
             "nvim-lua/plenary.nvim",
         },
         opts = {
+            notes_subdir = "~/vaults/01_Index",
+            new_notes_location = "~/vaults/01_Index",
+            note_id_func = function(title)
+                -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+                -- In this case a note with the title 'My new note' will be given an ID that looks
+                -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+                local suffix = ""
+                if title ~= nil then
+                    -- If title is given, transform it into valid file name.
+                    suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+                else
+                    -- If title is nil, just add 4 random uppercase letters to the suffix.
+                    for _ = 1, 4 do
+                        suffix = suffix .. string.char(math.random(65, 90))
+                    end
+                end
+                return tostring(os.time()) .. "-" .. suffix
+            end,
             ui = {
                 enable = false,
             },
             workspaces = {
                 {
-                    name = "LiteratureNotes",
-                    path = "~/vaults/00-Literature-Notes",
+                    name = "Notes",
+                    path = "~/vaults/01_Index",
                 },
                 {
-                    name = "FleetingNotes",
-                    path = "~/vaults/01-Fleeting-Notes",
+                    name = "Work",
+                    path = "~/vaults/02_Work",
                 },
                 {
-                    name = "PermanentNotes",
-                    path = "~/vaults/02-Permanent-Notes",
+                    name = "Private",
+                    path = "~/vaults/03_Private",
                 },
                 {
-                    name = "StructuredNotes",
-                    path = "~/vaults/03-Structured-Notes",
+                    name = "Areas",
+                    path = "~/vaults/04_Areas",
+                },
+                {
+                    name = "Archive",
+                    path = "~/vaults/05_Archive",
                 },
             },
             mappings = {},
