cb5649a00602405a8260cde1ebeaf06e6ae6b9a7
lighttiger2505 <lighttiger2505@gmail.com>
2025-10-21 21:34:56 +0900

    refactor(lsp): migrate from lspconfig to native vim.lsp.config
    Replace lspconfig.setup() calls with native vim.lsp.config assignments to use Neovim's built-in LSP configuration system.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -77,10 +77,9 @@ return {
             },
         },
         config = function(_, opts)
-            local lspconfig = require("lspconfig")
             for server, config in pairs(opts.servers) do
                 config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
+                vim.lsp.config[server] = config
             end
 
             vim.api.nvim_create_autocmd("LspAttach", {
