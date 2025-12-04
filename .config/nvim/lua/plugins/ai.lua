a5295609cbb5c86f39cb0f898d015690c9ead70a
lighttiger2505 <lighttiger2505@gmail.com>
2025-12-04 11:16:46 +0900

    config(ai): add ignore_warnings option to codecompanion setup

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -23,6 +23,7 @@ return {
         },
         config = function()
             require("codecompanion").setup({
+                ignore_warnings = true,
                 adapters = {
                     http = {
                         copilot = function()
