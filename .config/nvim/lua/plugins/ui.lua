923bca620c95cb0af9547989cb26cf3627e14b33
lighttiger2505 <lighttiger2505@gmail.com>
2026-01-15 20:01:21 +0900

    feat(nvim): add overlook.nvim plugin for peek definition functionality

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -207,4 +207,32 @@ return {
             { "<Leader>w", "<Cmd>NoNeckPain<CR>", mode = "n", desc = "NoNeckPain" },
         },
     },
+
+    {
+        "WilliamHsieh/overlook.nvim",
+        opts = {},
+        keys = {
+            {
+                "<leader>pd",
+                function()
+                    require("overlook.api").peek_definition()
+                end,
+                desc = "Overlook: Peek definition",
+            },
+            {
+                "<leader>pc",
+                function()
+                    require("overlook.api").close_all()
+                end,
+                desc = "Overlook: Close all popup",
+            },
+            {
+                "<leader>pu",
+                function()
+                    require("overlook.api").restore_popup()
+                end,
+                desc = "Overlook: Restore popup",
+            },
+        },
+    },
 }
