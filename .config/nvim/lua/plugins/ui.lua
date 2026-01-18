a59974b74958b9a3fc6caeeb9684526c2c118509
lighttiger2505 <lighttiger2505@gmail.com>
2026-01-18 15:05:09 +0900

    config(nvim): remove undo-glow search and comment keybindings and reduce animation duration

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -243,7 +243,7 @@ return {
         opts = {
             animation = {
                 enabled = true,
-                duration = 300,
+                duration = 200,
                 animation_type = "zoom",
                 window_scoped = true,
             },
@@ -307,92 +307,6 @@ return {
                 end,
                 mode = "n",
                 desc = "Paste above with highlight",
-                noremap = true,
-            },
-            {
-                "n",
-                function()
-                    require("undo-glow").search_next({
-                        animation = {
-                            animation_type = "strobe",
-                        },
-                    })
-                end,
-                mode = "n",
-                desc = "Search next with highlight",
-                noremap = true,
-            },
-            {
-                "N",
-                function()
-                    require("undo-glow").search_prev({
-                        animation = {
-                            animation_type = "strobe",
-                        },
-                    })
-                end,
-                mode = "n",
-                desc = "Search prev with highlight",
-                noremap = true,
-            },
-            {
-                "*",
-                function()
-                    require("undo-glow").search_star({
-                        animation = {
-                            animation_type = "strobe",
-                        },
-                    })
-                end,
-                mode = "n",
-                desc = "Search star with highlight",
-                noremap = true,
-            },
-            {
-                "#",
-                function()
-                    require("undo-glow").search_hash({
-                        animation = {
-                            animation_type = "strobe",
-                        },
-                    })
-                end,
-                mode = "n",
-                desc = "Search hash with highlight",
-                noremap = true,
-            },
-            {
-                "gc",
-                function()
-                    -- This is an implementation to preserve the cursor position
-                    local pos = vim.fn.getpos(".")
-                    vim.schedule(function()
-                        vim.fn.setpos(".", pos)
-                    end)
-                    return require("undo-glow").comment()
-                end,
-                mode = { "n", "x" },
-                desc = "Toggle comment with highlight",
-                expr = true,
-                noremap = true,
-            },
-            {
-                "gc",
-                function()
-                    require("undo-glow").comment_textobject()
-                end,
-                mode = "o",
-                desc = "Comment textobject with highlight",
-                noremap = true,
-            },
-            {
-                "gcc",
-                function()
-                    return require("undo-glow").comment_line()
-                end,
-                mode = "n",
-                desc = "Toggle comment line with highlight",
-                expr = true,
                 noremap = true,
             },
         },
