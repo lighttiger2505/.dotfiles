219fb9d2f09c7b5749bfb8fc0c27c1095920250a
lighttiger2505 <lighttiger2505@gmail.com>
2026-02-01 22:00:21 +0900

    feat(zsh): add direnv initialization hook

--- .zsh/command_hook.zsh
+++ .zsh/command_hook.zsh
@@ -17,6 +17,10 @@ if [ -e $HOME/.local/bin/mise ]; then
     eval "$($HOME/.local/bin/mise activate zsh)"
 fi
 
+if executable direnv; then
+    eval "$(git wt --init zsh)"
+fi
+
 # Google Cloud SDK.
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f $HOME/google-cloud-sdk/path.zsh.inc ]; then . $HOME/google-cloud-sdk/path.zsh.inc; fi
