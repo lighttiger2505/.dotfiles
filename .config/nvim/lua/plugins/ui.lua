60c045a8b251685416ad97e27ac3338048a0c385
lighttiger2505 <lighttiger2505@gmail.com>
2026-03-04 14:09:25 +0900

    refactor(nvim): replace undo-glow autocmds with smear-cursor plugin
    Remove CursorMoved, FocusGained, and CmdlineLeave autocmds from undo-glow configuration and add smear-cursor.nvim plugin for cursor animation effects.

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -292,53 +292,12 @@ return {
                     require("undo-glow").yank()
                 end,
             })
-
-            -- This only handles neovim instance and do not highlight when switching panes in tmux
-            vim.api.nvim_create_autocmd("CursorMoved", {
-                desc = "Highlight when cursor moved significantly",
-                callback = function()
-                    require("undo-glow").cursor_moved({
-                        animation = {
-                            animation_type = "slide",
-                        },
-                    })
-                end,
-            })
-
-            -- This will handle highlights when focus gained, including switching panes in tmux
-            vim.api.nvim_create_autocmd("FocusGained", {
-                desc = "Highlight when focus gained",
-                callback = function()
-                    ---@type UndoGlow.CommandOpts
-                    local opts = {
-                        animation = {
-                            animation_type = "slide",
-                        },
-                    }
-
-                    opts = require("undo-glow.utils").merge_command_opts("UgCursor", opts)
-                    local pos = require("undo-glow.utils").get_current_cursor_row()
-
-                    require("undo-glow").highlight_region(vim.tbl_extend("force", opts, {
-                        s_row = pos.s_row,
-                        s_col = pos.s_col,
-                        e_row = pos.e_row,
-                        e_col = pos.e_col,
-                        force_edge = opts.force_edge == nil and true or opts.force_edge,
-                    }))
-                end,
-            })
-
-            vim.api.nvim_create_autocmd("CmdlineLeave", {
-                desc = "Highlight when search cmdline leave",
-                callback = function()
-                    require("undo-glow").search_cmd({
-                        animation = {
-                            animation_type = "fade",
-                        },
-                    })
-                end,
-            })
         end,
+    },
+
+    {
+        "sphamba/smear-cursor.nvim",
+        event = "VeryLazy",
+        opts = {},
     },
 }
