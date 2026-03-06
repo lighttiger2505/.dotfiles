1bc529ec5ad9e2c113364cb4b3b5c2564105a1a3
lighttiger2505 <lighttiger2505@gmail.com>
2026-03-06 10:45:58 +0900

    feat(nvim): configure smear-cursor animation parameters

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -297,6 +297,14 @@ return {
     {
         "sphamba/smear-cursor.nvim",
         event = "VeryLazy",
-        opts = {},
+        opts = {
+            stiffness = 0.8,
+            trailing_stiffness = 0.6,
+            stiffness_insert_mode = 0.7,
+            trailing_stiffness_insert_mode = 0.7,
+            damping = 0.95,
+            damping_insert_mode = 0.95,
+            distance_stop_animating = 0.5,
+        },
     },
 }
