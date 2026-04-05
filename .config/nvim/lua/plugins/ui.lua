10d54442f8429b4d728f2ee65604920dcc370666
lighttiger2505 <lighttiger2505@gmail.com>
2026-04-05 18:12:44 +0900

    feat(nvim): add dropbar.nvim plugin with navigation keybinds

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -307,4 +307,15 @@ return {
             distance_stop_animating = 0.5,
         },
     },
+
+    {
+        "Bekaboo/dropbar.nvim",
+        event = "VeryLazy",
+        config = function()
+            local dropbar_api = require("dropbar.api")
+            vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+            vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+            vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+        end,
+    },
 }
