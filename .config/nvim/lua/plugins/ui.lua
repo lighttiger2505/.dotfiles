d1844d89f2f1951b83e05a6deb9199cc2041931b
lighttiger2505 <lighttiger2505@gmail.com>
2026-04-10 16:03:21 +0900

    refactor(nvim): remove smear-cursor.nvim plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -295,20 +295,6 @@ return {
     },
 
     {
-        "sphamba/smear-cursor.nvim",
-        event = "VeryLazy",
-        opts = {
-            stiffness = 0.8,
-            trailing_stiffness = 0.6,
-            stiffness_insert_mode = 0.7,
-            trailing_stiffness_insert_mode = 0.7,
-            damping = 0.95,
-            damping_insert_mode = 0.95,
-            distance_stop_animating = 0.5,
-        },
-    },
-
-    {
         "Bekaboo/dropbar.nvim",
         event = { "BufReadPre", "BufNewFile" },
         config = function()
