7427b324ad25a2fe5550a14a0fbdd06fc653e617
lighttiger2505 <lighttiger2505@gmail.com>
2026-04-10 16:03:08 +0900

    perf(nvim): optimize dropbar.nvim loading by changing event trigger

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -310,7 +310,7 @@ return {
 
     {
         "Bekaboo/dropbar.nvim",
-        event = "VeryLazy",
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             local dropbar_api = require("dropbar.api")
             vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
