79b1b5a82ce348797932d5536a529d8d1d8a0b4c
lighttiger2505 <lighttiger2505@gmail.com>
2026-04-12 15:36:15 +0900

    feat(tmux): add TPM plugin manager with which-key plugin

---
+++ .config/tmux/conf.d/tpm.tmux
@@ -0,0 +1,6 @@
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'alexwforsythe/tmux-which-key'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '~/.tmux/plugins/tpm/tpm'
--- .zsh/tmux.zsh
+++ .zsh/tmux.zsh
@@ -1,7 +1,14 @@
+# Install tmux plugin manager (tpm) if not already installed
+if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+fi
+
+# Automatically start tmux when login to shell
 if executable tmux; then
     tmux has-session -t ${TMUX_DEFAULT_SESSION} 2>/dev/null || tmux new-session -d -s ${TMUX_DEFAULT_SESSION}
 fi
 
+# Attach to tmux session if not already inside tmux and if the shell is interactive
 if [[ -o interactive ]] && [[ -z "${TMUX-}" ]]; then
     tmux attach -t ${TMUX_DEFAULT_SESSION}
 fi
