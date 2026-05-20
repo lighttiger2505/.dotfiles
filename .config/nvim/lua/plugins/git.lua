726f0ef76c630163d782c3e33cfaa9de107f5ba7
lighttiger2505 <lighttiger2505@gmail.com>
2026-05-20 16:54:59 +0900

    feat(nvim): add ghlite.nvim plugin for GitHub PR management with diffview integration

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -203,4 +203,33 @@ return {
         lazy = true,
         cmd = "CodeDiff",
     },
+
+    {
+        "daliusd/ghlite.nvim",
+        dependencies = { "sindrets/diffview.nvim" },
+        config = function()
+            require("ghlite").setup({
+                diff_tool = "diffview",
+            })
+        end,
+        keys = {
+            { "<leader>us", ":GHLitePRSelect<cr>", silent = true, desc = "PR Select" },
+            { "<leader>uo", ":GHLitePRCheckout<cr>", silent = true, desc = "PR Checkout" },
+            { "<leader>uv", ":GHLitePRView<cr>", silent = true, desc = "PR View" },
+            { "<leader>uu", ":GHLitePRLoadComments<cr>", silent = true, desc = "PR Load Comments" },
+            { "<leader>up", ":GHLitePRDiff<cr>", silent = true, desc = "PR Diff" },
+            { "<leader>ul", ":GHLitePRDiffview<cr>", silent = true, desc = "PR Diffview" },
+            { "<leader>ua", ":GHLitePRAddComment<cr>", silent = true, desc = "PR Add comment" },
+            {
+                "<leader>ua",
+                ":GHLitePRAddComment<cr>",
+                mode = "x",
+                silent = true,
+                desc = "PR Add comment",
+            },
+            { "<leader>uc", ":GHLitePRUpdateComment<cr>", silent = true, desc = "PR Update comment" },
+            { "<leader>ud", ":GHLitePRDeleteComment<cr>", silent = true, desc = "PR Delete comment" },
+            { "<leader>ug", ":GHLitePROpenComment<cr>", silent = true, desc = "PR Open comment" },
+        },
+    },
 }
