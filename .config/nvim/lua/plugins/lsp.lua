0bcbf0707e41444241ab4d9465a6bfcc13d15506
lighttiger2505 <lighttiger2505@gmail.com>
2026-05-30 11:17:23 +0900

    feat(nvim): add tiny-inline-diagnostic.nvim plugin and disable default virtual text diagnostics

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -233,4 +233,15 @@ return {
             },
         },
     },
+
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy",
+        priority = 1000,
+        config = function()
+            require("tiny-inline-diagnostic").setup()
+            -- Disable Neovim's default virtual text diagnostics
+            vim.diagnostic.config({ virtual_text = false })
+        end,
+    },
 }
